a0efe984041709532d6f39488d69e21d6d496efa
Amr M. Zedan <35425084+amrzedan2003@users.noreply.github.com>
2025-08-07 12:15:48 +0300

    Refactor event data classes to extend MyData with IDs
    Updated HeartbeatData, TaskData, and ReminderData classes to extend MyData with appropriate ID types (String, Long, Integer)

--- design-patterns/src/main/java/ps/exalt/events/types/ReminderEvent.java
+++ design-patterns/src/main/java/ps/exalt/events/types/ReminderEvent.java
@@ -3,13 +3,14 @@ package ps.exalt.events.types;
 import ps.exalt.events.CommonEvent;
 import ps.exalt.events.enums.EventType;
 import ps.exalt.events.enums.Priority;
+import ps.exalt.events.MyData;
 
 public class ReminderEvent extends CommonEvent {
     private final String reminderText;
     private final String recipient;
 
     public ReminderEvent(String reminderText, String recipient, Priority priority) {
-        super(EventType.REMINDER, priority, new ReminderData(reminderText, recipient));
+        super(EventType.REMINDER, priority, new ReminderData(3, reminderText, recipient));
         this.reminderText = reminderText;
         this.recipient = recipient;
     }
@@ -29,12 +30,14 @@ public class ReminderEvent extends CommonEvent {
 
     /**
      * DTO for reminder information
+     * ID must be remain as int
      */
-    public static class ReminderData {
+    public static class ReminderData extends MyData<Integer> {
         private final String text;
         private final String recipient;
 
-        public ReminderData(String text, String recipient) {
+        public ReminderData(int id, String text, String recipient) {
+            super(id);
             this.text = text;
             this.recipient = recipient;
         }
